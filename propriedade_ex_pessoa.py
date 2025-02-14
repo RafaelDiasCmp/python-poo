361e6b971e6ccb369cce2e9aa31ce02d6821e952
Rafael Dias Campos <73750287+RafaelDiasCmp@users.noreply.github.com>
2025-02-13 19:02:07 -0500

    fundamentos de encapsulamento em poo com python

---
+++ propriedade_ex_pessoa.py
@@ -0,0 +1,25 @@
+class Pessoa:
+    def __init__(self, nome, ano_nascimento):
+        self._nome = nome
+        self._ano_nascimento = ano_nascimento
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @property
+    def idade(self):
+        _ano_atual = 2025
+        return _ano_atual - self._ano_nascimento
+    
+
+    def get_nome(self):
+        return self._nome
+    
+    def get_idade(self):
+        return 2025 - self._ano_nascimento
+    
+
+    
+pessoa = Pessoa('João', 1990)
+print(f"Nome: {pessoa.nome} \t Idade: {pessoa.idade}")
